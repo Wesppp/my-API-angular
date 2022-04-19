5dca7db9c618a005257dc4117442cbe7ac07bf59
Wesppp <ooo.malina.03@mail.ru>
2022-04-19 13:26:17 +0300

    implemented the model

--- src/app/server/myAPI.js
+++ src/app/server/myAPI.js
@@ -1,30 +1,19 @@
 const express = require("express");
-const fs = require("fs");
+const Subject = require('./model.js')
 
 const cors = require('cors')
 const app = express();
 const jsonParser = express.json();
 const port = 3002;
 
-const filePath = "subjects.json";
-app.get("/api/subjects", function (req, res) {
-  const content = fs.readFileSync(filePath, "utf8");
-  const subjects = JSON.parse(content);
+app.get("/api/subjects", async function (req, res) {
+  const subjects = await Subject.getAll()
   res.send(subjects);
 });
 
-app.get("/api/subjects/:id", function (req, res) {
+app.get("/api/subjects/:id", async function (req, res) {
   const id = req.params.id;
-  const content = fs.readFileSync(filePath, "utf8");
-  const subjects = JSON.parse(content);
-  let subject = null;
-
-  for (let i = 0; i < subjects.length; i++) {
-    if (subjects[i].id == id) {
-      subject = subjects[i];
-      break;
-    }
-  }
+  let subject = await Subject.getById(id)
 
   if (subject) {
     res.send(subject);
@@ -33,44 +22,24 @@ app.get("/api/subjects/:id", function (req, res) {
   }
 });
 
-app.post("/api/subjects", jsonParser, function (req, res) {
+app.post("/api/subjects", jsonParser, async function (req, res) {
 
   if (!req.body) return res.sendStatus(400);
 
-  const subjectName = req.body.name;
-  let subject = {name: subjectName};
-
-  let data = fs.readFileSync(filePath, "utf8");
-  let subjects = JSON.parse(data);
-
+  let subjects = await Subject.getAll()
   const id = Math.max.apply(Math, subjects.map(function (o) {
     return o.id;
   }))
-  subject.id = id + 1;
-  subjects.push(subject);
-  data = JSON.stringify(subjects);
-  fs.writeFileSync("subjects.json", data);
+
+  let subject = new Subject(req.body.name, id+1)
+  await subject.save()
   res.send(subject);
 });
 
-app.delete("/api/subjects/:id", function (req, res) {
+app.delete("/api/subjects/:id", async function (req, res) {
+  let subject = await Subject.deleteSubject(req.params.id)
 
-  const id = req.params.id;
-  let data = fs.readFileSync(filePath, "utf8");
-  let subjects = JSON.parse(data);
-  let index = -1;
-
-  for (let i = 0; i < subjects.length; i++) {
-    if (subjects[i].id == id) {
-      index = i;
-      break;
-    }
-  }
-  if (index > -1) {
-    const subject = subjects.splice(index, 1)[0];
-    data = JSON.stringify(subjects);
-    fs.writeFileSync("subjects.json", data);
-
+  if (subject) {
     res.send(subject);
   } else {
     res.status(404).send();
@@ -78,26 +47,11 @@ app.delete("/api/subjects/:id", function (req, res) {
 });
 
 app.put("/api/subjects", jsonParser, function (req, res) {
-
   if (!req.body) return res.sendStatus(400);
-
-  const subjectId = req.body.id;
-  const subjectName = req.body.name;
-
-  let data = fs.readFileSync(filePath, "utf8");
-  const subjects = JSON.parse(data);
-  let subject;
-  for (let i = 0; i < subjects.length; i++) {
-    if (subjects[i].id == subjectId) {
-      subject = subjects[i];
-      break;
-    }
-  }
+  let subject = new Subject(req.body.name, req.body.id)
 
   if (subject) {
-    subject.name = subjectName;
-    data = JSON.stringify(subjects);
-    fs.writeFileSync("subjects.json", data);
+    subject = Subject.updateSubject(subject)
     res.send(subject);
   } else {
     res.status(404).send(subject);
